781b9de274eddd367ceca119f73295d9d96d98e8
spikereloaded <lewis.198804@gmail.com>
2026-05-26 22:42:47 +0100

    fully responsive navigation, mobiel first breakpoints. Added css files for unfinished pages.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-//hides the countdown timer until the countdown has been calculated and set
 
+const arrow = document.getElementById("scroll-arrow");
 
 // toggles the play state of the video playing in the background of panel 3
 function toggleVideo() {
@@ -48,3 +48,27 @@ var x = setInterval(function() {
     document.getElementById("countdown-text").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+//checks to see if the scroll arrow is visible in the viewport currently
+//if the scroll arrow moves out of the viewport, it will hide it
+//ensures that the scroll arrow is not visible when scrolling up and down
+let observer = new IntersectionObserver(function (entries, observer) {
+  let onScreen = entries[0]["isIntersecting"];
+  if (!onScreen && arrow.hidden == false) {
+    console.log("arrow hiddden");
+    arrow.hidden = true;
+  }
+});
+
+observer.observe(arrow);
+
+//checks the scroll position on the Y. axis to see if the viewport is 
+//a set distance from the top (e.g. 0 = very top, 50 = 50 px from the top). 
+// If it is, and the scroll arrow is hidden, make the scroll arrow visible again.
+window.addEventListener("scroll", () => {
+  if (window.scrollY < 50 && arrow.hidden == true) {
+    console.log("arrow visible");
+    arrow.hidden = false;
+  }
+});
--- assets/js/site-wide.js
+++ assets/js/site-wide.js
@@ -1,30 +1,7 @@
-const arrow = document.getElementById("scroll-arrow");
 const burger = document.getElementById("mobile-nav-toggle");
 const navigationMenu = document.getElementById("navigation");
+let screenWidth = screen.width;
 
-
-//checks to see if the scroll arrow is visible in the viewport currently
-//if the scroll arrow moves out of the viewport, it will hide it
-//ensures that the scroll arrow is not visible when scrolling up and down
-let observer = new IntersectionObserver(function (entries, observer) {
-  let onScreen = entries[0]["isIntersecting"];
-  if (!onScreen && arrow.hidden == false) {
-    console.log("arrow hiddden");
-    arrow.hidden = true;
-  }
-});
-
-observer.observe(arrow);
-
-//checks the scroll position on the Y. axis to see if the viewport is 
-//a set distance from the top (e.g. 0 = very top, 50 = 50 px from the top). 
-// If it is, and the scroll arrow is hidden, make the scroll arrow visible again.
-window.addEventListener("scroll", () => {
-  if (window.scrollY < 50 && arrow.hidden == true) {
-    console.log("arrow visible");
-    arrow.hidden = false;
-  }
-});
 
 // function to scroll viewport to a specific panel
 function scrollToPanel(panelId){
@@ -34,13 +11,21 @@ function scrollToPanel(panelId){
     window.scrollBy({top:panelLocation.top, behavior: "smooth"} );
 }
 
-//function to toggle visiblity of mobile first navigation menu
+// //function to toggle visiblity of mobile first navigation menu
+// function toggleNav(){
+//   if(navigationMenu.classList.contains("hidden")){
+//     navigationMenu.classList.remove("hidden");
+//     navigationMenu.classList.add("visible-block");
+//   }else{
+//     navigationMenu.classList.remove("visible-block");
+//     navigationMenu.classList.add("hidden");
+//   }
+// }
+
 function toggleNav(){
-  if(navigationMenu.classList.contains("hidden")){
-    navigationMenu.classList.remove("hidden");
-    navigationMenu.classList.add("visible-block");
-  }else{
-    navigationMenu.classList.remove("visible-block");
-    navigationMenu.classList.add("hidden");
+  if(navigationMenu.style.display == "none"){
+    navigationMenu.style.display = "block";
+  } else {
+    navigationMenu.style.display = "none";
   }
 }
