068878d671fcbe20291b183681011bf6041ba518
spikereloaded <lewis.198804@gmail.com>
2026-05-27 22:03:12 +0100

    added JS to auto close nav menu (mobile)when not focussed. Start accessiblilty sweep. Set hero image size unit correctly.

--- assets/js/site-wide.js
+++ assets/js/site-wide.js
@@ -1,5 +1,11 @@
 const burger = document.getElementById("mobile-nav-toggle");
 const navigationMenu = document.getElementById("navigation");
+
+//listens for click events on the main content element 
+const mainContent = document.getElementById("main-content-container");
+mainContent.addEventListener("click",checkNavMenu);
+
+
 let screenWidth = screen.width;
 
 
@@ -11,21 +17,25 @@ function scrollToPanel(panelId){
     window.scrollBy({top:panelLocation.top, behavior: "smooth"} );
 }
 
-// //function to toggle visiblity of mobile first navigation menu
-// function toggleNav(){
-//   if(navigationMenu.classList.contains("hidden")){
-//     navigationMenu.classList.remove("hidden");
-//     navigationMenu.classList.add("visible-block");
-//   }else{
-//     navigationMenu.classList.remove("visible-block");
-//     navigationMenu.classList.add("hidden");
-//   }
-// }
 
 function toggleNav(){
   if(navigationMenu.style.display == "none"){
     navigationMenu.style.display = "block";
   } else {
+    navigationMenu.style.display = "none";
+  }
+}
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >50 && navigationMenu.style.display == "block") {
+    console.log("shut menu");
+    navigationMenu.style.display = "none";
+  }
+});
+
+//closign the nav menu automatically when user has it open but clicks/presses outside of nav
+function checkNavMenu(){
+  if(navigationMenu.style.display == "block"){
     navigationMenu.style.display = "none";
   }
 }
