5cd216cda343e8dfcb69f33c0a01bead38246369
spikereloaded <lewis.198804@gmail.com>
2026-05-31 22:24:46 +0100

    further fine tuning video panel. Added visual feedback when skipping forwards and improved button styling.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,8 @@
 const kennedyText = document.getElementById("kennedy-reference-content");
 const videoControls = document.getElementById("video-controls");
 const panelVideo = document.getElementById("kennedy-video");
-
-
+const videoOverlay = document.getElementById("controls-feedback-overlay");
+const skipText = document.querySelector("#overlay-skip-text");
 
 
 // toggles the play state of the video playing in the background of panel 3
@@ -16,19 +16,19 @@ function toggleVideo() {
     kennedyText.classList.toggle("slide-out");
     videoControls.classList.toggle("hidden");
     document.getElementById("play-icon").classList.toggle("hidden-element");
-    document.getElementById("stop-icon").classList.toggle("hidden-element"); 
+    document.getElementById("stop-icon").classList.toggle("hidden-element");
     //add in feedback class to show button was pressed
     document.getElementById("video-playback-toggle-button").classList.toggle("pressed-control");
     panelVideo.muted = false;
     panelVideo.play();
-   
+
     scrollToPanel("kennedy-box");
   } else {
     panelVideo.pause();
     videoControls.classList.toggle("hidden");
     kennedyText.classList.toggle("slide-out");
     kennedyText.classList.toggle("slide-in");
-   
+
     document.getElementById("play-icon").classList.toggle("hidden-element");
     document.getElementById("stop-icon").classList.toggle("hidden-element");
     //remove feedback class to show button was pressed
@@ -37,9 +37,26 @@ function toggleVideo() {
 }
 
 function skipForward() {
+  if (videoOverlay.classList.contains("overlay-animation")) {
+    videoOverlay.classList.remove("overlay-animation");
+  }
+  if (skipText.classList.contains("visible-element")) {
+    skipText.classList.remove("visible-element");
+  }
+  
   panelVideo.currentTime += 10;
+  skipText.classList.toggle("visible-element");
+  void videoOverlay.offsetWidth;
+  videoOverlay.classList.add("overlay-animation");
+
 }
 
+function resetOverlayAnimation() {
+  // alert("animation finished");
+  skipText.classList.toggle("visible-element");
+  videoOverlay.classList.remove("overlay-animation");
+
+}
 //https://www.w3schools.com/howto/howto_js_countdown.asp
 // // Set the date we're counting down to
 // var countDownDate = new Date("Jan 5, 2028 15:37:25").getTime();
@@ -91,19 +108,20 @@ document.getElementById("volume-down").addEventListener("click", function () {
   if (newVolume >= 0) {
     panelVideo.volume = panelVideo.volume - 0.2;
     console.log("volume", newVolume);
+
+
   } else {
     console.log("Min volume hit");
   }
-
-})
+});
 
 //add in eventlistener that detects click on panelVideo and triggers toggleVideo function
 
-  panelVideo.addEventListener("click", () => {
-    if(panelVideo.paused === false){
-      toggleVideo();
-    }
-  });
+panelVideo.addEventListener("click", () => {
+  if (panelVideo.paused === false) {
+    toggleVideo();
+  }
+});
 
 
 
@@ -111,3 +129,5 @@ document.getElementById("volume-down").addEventListener("click", function () {
 document.getElementById("skip-forward").addEventListener("click", skipForward);
 //toggles the play state of the video when the play/pause button is clicked.
 document.getElementById("video-playback-toggle-button").addEventListener("click", toggleVideo);
+
+videoOverlay.addEventListener("animationend", resetOverlayAnimation)
